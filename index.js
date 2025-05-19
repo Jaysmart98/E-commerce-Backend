7c25ec0a65638e3a3e287ee2cb63eed9ea9e57c8
Jaysmart <ogunbunmijoshua60@gmail.com>
2025-05-19 12:12:27 +0100

    Initialize Express server with user routes and MongoDB connection; update dependencies

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const userRouter = require("./routes/user.route")
+const cors = require("cors")
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose")
+
+const port = process.env.PORT || 5150 
+
+
+app.use(express.urlencoded({ extended: true, limit: "100mb" }))
+app.use(express.json())
+app.use(cors())
+app.use("/user", userRouter)
+require("dotenv").config()
+const URI = process.env.uri || undefined
+mongoose.connect(URI)
+
+
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello')
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+  });
