e42cb5b504bb5cc6ef60fe8d15f40d4d1eb1016a
Jaysmart <ogunbunmijoshua60@gmail.com>
2025-05-21 23:53:00 +0100

    Add unique constraints to user model fields

--- models/user.model.js
+++ models/user.model.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema({
-    Name: { type: String, required: true },
-    Email: { type: String, required: true, },
+    Name: { type: String, required: true, unique: true },
+    Email: { type: String, required: true, unique: true },
     Password: { type: String, required: true },
     dateAdded: { type: Date, default: Date.now() },
     lastLogin: { type: Date } 
